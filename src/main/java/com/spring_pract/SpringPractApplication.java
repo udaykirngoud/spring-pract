50e068207bbb40ecd8c11e85b3f4829d7db10403
uday kiran chanagani <udaykiranchanagani@gmail.com>
2026-02-19 21:21:46 +0530

    first commit

---
+++ src/main/java/com/spring_pract/SpringPractApplication.java
@@ -0,0 +1,13 @@
+package com.spring_pract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_pract/SpringPractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_pract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
