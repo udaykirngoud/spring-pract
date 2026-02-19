e87289ea71eda1edbee10636c3f21a317cdbb36a
uday kiran chanagani <udaykiranchanagani@gmail.com>
2026-02-19 21:28:26 +0530

    second commit

---
+++ src/main/java/com/spring_pract/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.spring_pract.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "hi this is uday";
+    }
+}
